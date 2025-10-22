6643db9cfe318fbf91845fd728e0abf0c5c203da
Treasure Uzoma <treasureuzoma650@gmail.com>
2025-10-22 17:58:22 +0100

    first commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,38 @@
-import { Button } from "@workspace/ui/components/button"
+"use client";
 
-export default function Page() {
+import { ModeToggle } from "@/components/theme-toggle";
+import { Button } from "@workspace/ui/components/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@workspace/ui/components/select";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export default function HomePage() {
+  const [value, setValue] = useState("go");
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold">Shadcn in turborepo :)</h1>
+        <Button size="sm" onClick={() => toast.info("You clicked me :)")}>
+          Click me
+        </Button>
+        <ModeToggle />
+        <Select value={value} onValueChange={setValue}>
+          <SelectTrigger className="w-[200px]">
+            <SelectValue placeholder="Best Language" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="go">Go</SelectItem>
+            <SelectItem value="ts">Typescript</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
     </div>
-  )
+  );
 }
